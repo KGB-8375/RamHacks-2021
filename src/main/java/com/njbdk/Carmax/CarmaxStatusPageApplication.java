32b204378d7711f97089b9b07011657be31efd31
KGB-8375 <27stopittj@gmail.com>
2021-09-25 14:11:10 -0400

    spring stuff

---
+++ src/main/java/com/njbdk/Carmax/CarmaxStatusPageApplication.java
@@ -0,0 +1,13 @@
+package com.njbdk.Carmax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarmaxStatusPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarmaxStatusPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/njbdk/Carmax/CarmaxStatusPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.njbdk.Carmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarmaxStatusPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
